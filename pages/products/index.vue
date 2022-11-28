222f374ca31af0b641b67405ce51ec0fa5685fd1
IvanPazhgin <ivan.pazhgin@gmail.com>
2022-11-28 18:49:51 +0300

    добавил пагинацию

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -12,7 +12,18 @@
           :product="product">
         </product-page>
       </v-row>
-<!--    <product-dialog />-->
+
+    <div class="page__wrapper">
+      <div
+        v-for="pageNumber in $store.getters['goods/totalPages']"
+        :key="pageNumber"
+        class="page"
+        :class="{'current-page': pageNumber === $store.state.goods.currentPage}"
+        @click="changePage(pageNumber)"
+      >
+        {{ pageNumber }}
+      </div>
+    </div>
   </v-container>
 </template>
 
@@ -32,7 +43,6 @@ export default {
 
   data: () => ({
     searchQuery: '',
-    // dialogVisible: false,
   }),
 
   computed: {
@@ -43,6 +53,12 @@ export default {
       return this.goods.filter(product => product.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
     }
   },
+  methods: {
+    async changePage(pageNumber) {
+      this.$store.commit('goods/setCurrentPage', pageNumber)
+      await this.$store.dispatch('goods/fetch')
+    }
+  }
 }
 </script>
 
@@ -51,6 +67,21 @@ export default {
   width: 100%;
   border: 1px solid teal;
   padding: 10px 15px;
+  /*margin-top: 15px;*/
+  margin-bottom: 10px;
+}
+.page__wrapper {
+  display: flex;
   margin-top: 15px;
+  flex-wrap: wrap;
+}
+.page {
+  /*border: 1px solid black;*/
+  color: blue;
+  padding: 10px;
+}
+.current-page {
+  border: 1px solid teal;
+  color: black;
 }
 </style>
--- store/goods.js
+++ store/goods.js
@@ -1,23 +1,44 @@
 // единственная константа, которая будет функцией, чтобы избежать мутаций
 export const state = () => ({
-  goods: []
+  goods: [],
+  currentPage: 1,
+  limitOnPage: 12,
+  totalPages: 0,
 })
 
 // изменяем state
 export const mutations = {
   setGoods(state, goods) {
     state.goods = goods
+  },
+  setCurrentPage(state, currentPage) {
+    state.currentPage = currentPage
+  },
+  setTotalPages(state, totalPages) {
+    state.totalPages = totalPages
   }
 }
 
 // грузим данные с сервера
 export const actions = {
-  async fetch({commit}) {
-    const goods = await this.$axios.$get('https://jsonplaceholder.typicode.com/photos?_limit=11')
+  async fetch({commit, state}) {
+    const goods = await this.$axios.$get('https://jsonplaceholder.typicode.com/photos', {
+      params: {
+        _page: state.currentPage,
+        _limit: state.limitOnPage,
+      }
+    })
     commit('setGoods', goods)
+
+    const x_total_count = await this.$axios.get('https://jsonplaceholder.typicode.com/photos?_limit=1')
+    const totalPages = Math.ceil(x_total_count.headers['x-total-count'] / state.limitOnPage)
+    commit('setTotalPages', totalPages)
   }
 }
 
 export const getters = {
-  goods: state => state.goods
+  goods: state => state.goods,
+  // currentPage: state => state.currentPage,
+  totalPages: state => state.totalPages,
+
 }
